08386625e44f3d21dc4f64017c4cb487746bce28
Hawa Drammeh <hawad416@gmail.com>
2021-12-25 16:27:01 -0800

    created routing for elemnts module

--- src/app/elements/elements-routing.module.ts
+++ src/app/elements/elements-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ElementsHomeComponent } from './elements-home/elements-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'elements', component: ElementsHomeComponent }];
 
+//real meat and potatos for routing lol
+//need to set up a routing rule
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ElementsRoutingModule { }
+export class ElementsRoutingModule {}
