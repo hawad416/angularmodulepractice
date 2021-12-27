5adc4b5f880917dcf6316a2500ba00158ae4110d
Hawa Drammeh <hawad416@gmail.com>
2021-12-26 21:05:36 -0800

    created table component, and binded headers

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-collections-home',
   templateUrl: './collections-home.component.html',
-  styleUrls: ['./collections-home.component.css']
+  styleUrls: ['./collections-home.component.css'],
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    { name: 'James', age: 24, job: 'designer' },
+    { name: 'jill', age: 25, job: 'engineer' },
+    { name: 'hawa', age: 20, job: 'engineer' },
+  ];
 
-  constructor() { }
+  headers = [
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' },
+  ];
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
